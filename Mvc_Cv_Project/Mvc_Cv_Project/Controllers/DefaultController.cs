6f7c0ed3328d0e0a4a803a66eda1d12c44dfe3d1
Samet Tüzüner <samettznerr@gmail.com>
2023-02-05 15:54:40 +0300

    C# Mvc Cv
    Admin Panelli Dinamik CV Sitesi

---
+++ Mvc_Cv_Project/Mvc_Cv_Project/Controllers/DefaultController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.Ajax.Utilities;
+using Mvc_Cv_Project.Models.Entity;
+
+namespace Mvc_Cv_Project.Controllers
+{
+    [AllowAnonymous]
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        DbCvEntities1 db = new DbCvEntities1();
+        public ActionResult Index()
+        {
+            var degerler = db.TblHakkimda.ToList();
+            return View(degerler);
+        }
+        public ActionResult Hakkımda()
+        {
+            var s = db.TblHakkimda.ToList();
+            return View(s);
+        }
+
+        public PartialViewResult Egitim()
+        {
+            var egitim = db.TblEgitimlerim.ToList();
+            return PartialView(egitim);
+        }
+        public PartialViewResult Yetenekler()
+        {
+            var yetenek = db.TblYetenekler.ToList();
+            return PartialView(yetenek);
+        }
+        public PartialViewResult Sertifikalar()
+        {
+            var sertifika = db.TblSertifikalarim.ToList();
+            return PartialView(sertifika);
+        }
+        public PartialViewResult Projeler()
+        {
+            var proje = db.TblDeneyimlerim.ToList();
+            return PartialView(proje);
+        }
+        public PartialViewResult Tecrubelerim()
+        {
+            var tecrube = db.TblTecrubelerim.ToList();
+            return PartialView(tecrube);
+        }
+        [HttpGet]
+        public PartialViewResult iletisim()
+        {
+            var iletisimm = db.TblHakkimda.ToList();
+            return PartialView();
+        }
+        [HttpPost]
+        public PartialViewResult iletisim(Tbliletisim t)
+        {
+            t.Tarih = DateTime.Parse(DateTime.Now.ToShortDateString());
+            db.Tbliletisim.Add(t);
+            db.SaveChanges();
+            return PartialView();
+        }
+
+    }
+}
---
+++ Mvc_Cv_Project/Mvc_Cv_Project/Controllers/DeneyimController.cs
@@ -0,0 +1,56 @@
+﻿using Mvc_Cv_Project.Models.Entity;
+using Mvc_Cv_Project.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mvc_Cv_Project.Controllers
+{
+    public class DeneyimController : Controller
+    {
+        // GET: Deneyim
+
+        DeneyimRepository repo = new DeneyimRepository();
+        public ActionResult Index()
+        {
+            var degerler = repo.List();
+            return View(degerler);
+        }
+        [HttpGet]
+        public ActionResult DeneyimEkle()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult DeneyimEkle(TblDeneyimlerim p)
+        {
+            repo.Add(p);
+            return RedirectToAction("Index");
+        }
+        public ActionResult DeneyimSil(int id)
+        {
+            TblDeneyimlerim t = repo.Find(x => x.ID == id);
+             repo.Delete(t);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult DeneyimGetir(int id)
+        {
+            TblDeneyimlerim t = repo.Find(x => x.ID == id);
+            return View(t);
+        }
+        [HttpPost]
+        public ActionResult DeneyimGetir(TblDeneyimlerim p)
+        {
+            TblDeneyimlerim t = repo.Find(x => x.ID == p.ID);
+            t.Baslik = p.Baslik;
+            t.Aciklama = p.Aciklama;
+            t.ResimLink = p.ResimLink;
+            t.ProjeLink = p.ProjeLink;
+            repo.TUpdate(t);
+            return RedirectToAction("Index");
+        }
+    }
+}
